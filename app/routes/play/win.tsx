99f811576fc89d0b2d6d059389b02a7a68ed9b72
chidiebube <gpanyaogu@gmail.com>
2022-01-25 15:55:20 +0000

    show words and points when user wins

--- app/routes/play/win.tsx
+++ app/routes/play/win.tsx
@@ -1,11 +1,20 @@
+import { LoaderFunction, useLoaderData } from "remix";
 import Modal from "~/components/Modal";
 
+export let loader: LoaderFunction = async ({ request }) => {
+    const url = new URL(request.url);
+    let score = url.searchParams.get("score");
+    return score;
+};
+
 const Win = () => {
+    const score = useLoaderData();
+
     return (
         <Modal
             icon={"✓"}
             title={"YIPEE!!"}
-            message={"Congratulations! You have WON the game!"}
+            message={`Congratulations! You have WON this round ${score && `and gained ${score} points`}!`}
             iconStyle={"text-green-600 text-8xl m-0 p-0"}
         />
     );
